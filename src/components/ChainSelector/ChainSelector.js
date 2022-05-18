1a32e0f0b3aefdb195fb07bd8cce6b3bcdc097c5
dragondev-king <dragondev1017@gmail.com>
2022-05-19 00:11:32 +0900

    Finalize the Header component

--- src/components/ChainSelector/ChainSelector.js
+++ src/components/ChainSelector/ChainSelector.js
@@ -1,9 +1,27 @@
+import { useState, useCallback } from 'react'
+import { MenuItem, Select, SvgIcon } from '@material-ui/core'
 import './style.scss'
+import EthereumLogo from '../../assets/ethereum-logo.svg'
+import FantomLogo from '../../assets/fantom-logo.svg'
+import PolygonLogo from '../../assets/polygon-logo.svg'
+import BNBLogo from '../../assets/bsc-logo.svg'
+import AvancheLogo from '../../assets/avalanche-logo.svg'
 
 const ChainSelector = () => {
+  const [chain, setChain] = useState('ethereum')
+
+  const handleChainChange = useCallback((e) =>{
+    setChain(e.target.value)
+  }, [setChain])
   return (
-    <div className='selector'>
-      Selector
+    <div className='select-wrapper'>
+      <Select value={chain} onChange={handleChainChange} className='select' variant='outlined'>
+        <MenuItem value='ethereum'><img src={EthereumLogo} alt=''/><span>Ethereum</span></MenuItem>
+        <MenuItem value='fantom'><img src={FantomLogo} alt=''/><span>Fantom</span></MenuItem>
+        <MenuItem value='polygon'><img src={PolygonLogo} alt=''/><span>Polygon</span></MenuItem>
+        <MenuItem value='bnb'><img src={BNBLogo} alt=''/><span>BNB Chain</span></MenuItem>
+        <MenuItem value='avalanche'><img src={AvancheLogo} alt=''/><span>Avalanche</span></MenuItem>
+      </Select>
     </div>
   )
 }

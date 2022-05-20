3a78b8c36de03137e587d656f236f7e8e91706f5
dragondev-king <dragondev1017@gmail.com>
2022-05-21 00:19:36 +0900

    WIP: total view chart

--- src/routes/Dashboard/components/TotalViewChart/TotalViewChart.js
+++ src/routes/Dashboard/components/TotalViewChart/TotalViewChart.js
@@ -1,10 +1,26 @@
+import { VictoryChart, VictoryLine, VictoryTheme } from "victory";
+import './style.scss'
 
 const TotalViewChart = () => {
   return (
-    <div className="">
-      TotalViewChart
+    <div className="chart-wrapper">
+      <VictoryChart theme={VictoryTheme.material}>
+        <VictoryLine
+          style={{
+            data: { stroke: "#00FF57" },
+            parent: { border: "1px solid #eee" },
+          }}
+          data={[
+            { x: 1, y: 2 },
+            { x: 2, y: 3 },
+            { x: 3, y: 5 },
+            { x: 4, y: 4 },
+            { x: 5, y: 7 },
+          ]}
+        />
+      </VictoryChart>
     </div>
-  )
-}
+  );
+};
 
-export default TotalViewChart
+export default TotalViewChart;

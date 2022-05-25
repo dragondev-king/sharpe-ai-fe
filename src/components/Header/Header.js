1b16278ed200298ff61b45ab479a8b32e2c266a1
dragondev-king <dragondev1017@gmail.com>
2022-05-25 23:29:07 +0900

    fix staying navbar hover color

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,15 +5,16 @@ import { NavbarContext } from '../../context/NavbarContext'
 import ChainSelector from '../ChainSelector'
 import ConnectButton from '../ConnectButton'
 import './style.scss'
+import { menus } from '../../utils/helpers'
 import { ReactComponent as Logo } from '../../assets/svgs/logo.svg'
 
 const Header = () => {
-  const { headerText } = useContext(NavbarContext)
+  const { headerText, setHeaderText } = useContext(NavbarContext)
 
   return (
     <div className="header">
       <div className='header-left'>
-        <Link to='/'>
+        <Link to='/' onClick={() => setHeaderText(menus.dashboard)}>
           <Logo className='logo'/>
         </Link>
         <div className='dashboard-text'>{headerText}</div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,32 +5,46 @@ import { MenuItem, MenuList } from '@material-ui/core'
 import { NavbarContext } from '../../context/NavbarContext'
 import SocialIcons from '../SocialIcons'
 import './style.scss'
+import { menus } from '../../utils/helpers'
 import { ReactComponent as CategoryIcon } from '../../assets/svgs/category.svg'
 import { ReactComponent as FolderIcon } from '../../assets/svgs/folder.svg'
 import {ReactComponent as ChatIcon } from '../../assets/svgs/chat.svg'
 import { ReactComponent as HomeIcon } from '../../assets/svgs/home.svg'
 
 const Navbar = () => {
-  const {subIsOpened, setSubIsOpened, setHeaderText} = useContext(NavbarContext)
+  const {subIsOpened, setSubIsOpened,headerText, setHeaderText} = useContext(NavbarContext)
   
   const handleNavClick = useCallback((e) => {
     setSubIsOpened(false)
     setHeaderText(e.target.innerText)
   }, [setSubIsOpened, setHeaderText])
   
-  const handleSubClick = useCallback(() => {
+  const handleSubClick = useCallback((e) => {
     setSubIsOpened(!subIsOpened)
-  }, [subIsOpened, setSubIsOpened])
+    setHeaderText('Taurus')
+  }, [subIsOpened, setSubIsOpened, setHeaderText])
 
   const handleSubItemClick = useCallback((e) => {
     setHeaderText(e.target.innerText)
   }, [setHeaderText])
 
+  const handleClassName = ((menuName) => {
+    if(headerText === menuName) return 'selected'
+    else return ''
+  })
+
+  const handleSubClassName = (() => {
+    const subNames = [menus.taurus, menus.phantom, menus.helios, menus.vela, menus.centauri]
+    console.log(subNames.includes(headerText), '----')
+    if(subNames.includes(headerText)) return 'selected'
+    else return ''
+  })
+
   return (
     <div className="navbar">
       <MenuList>
-        <MenuItem onClick={handleNavClick} component={Link} to='/dashboard' key={'kkk'} ><HomeIcon className='menuicon'/><span>Dashboard</span></MenuItem>
-        <MenuItem onClick={handleSubClick} component={Link} to='/stratigies'>
+        <MenuItem onClick={handleNavClick} component={Link} to='/dashboard' className={handleClassName(menus.dashboard)}><HomeIcon className='menuicon'/><span>Dashboard</span></MenuItem>
+        <MenuItem onClick={handleSubClick} component={Link} to='/stratigies' className={handleSubClassName()}>
           <CategoryIcon className='menuicon' />
           <span>Stratigies</span>
           <svg className="MuiSvgIcon-root MuiSelect-icon MuiSelect-iconOutlined">
@@ -39,15 +53,15 @@ const Navbar = () => {
         </MenuItem>
           <div className='sub-group' style={{display: subIsOpened ? 'block' : 'none'}}>
             <MenuList>
-              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/taurus'>Taurus</MenuItem>
-              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/phantom'>Phantom</MenuItem>
-              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/helios'>Helios</MenuItem>
-              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/vela'>Vela</MenuItem>
-              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/centauri'>Centauri</MenuItem>
+              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/taurus' className={handleClassName(menus.taurus)}>Taurus</MenuItem>
+              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/phantom' className={handleClassName(menus.phantom)}>Phantom</MenuItem>
+              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/helios' className={handleClassName(menus.helios)}>Helios</MenuItem>
+              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/vela' className={handleClassName(menus.vela)}>Vela</MenuItem>
+              <MenuItem onClick={handleSubItemClick} component={Link} to='/stratigies/centauri' className={handleClassName(menus.centauri)}>Centauri</MenuItem>
             </MenuList>
           </div>
-        <MenuItem onClick={handleNavClick} component={Link} to='/indices'><FolderIcon className='menuicon' /><span>Indices</span></MenuItem>
-        <MenuItem onClick={handleNavClick} component={Link} to='/feedback'><ChatIcon className='menuicon' /><span>Issue/Feedback</span></MenuItem>
+        <MenuItem onClick={handleNavClick} component={Link} to='/indices' className={handleClassName(menus.indicies)}><FolderIcon className='menuicon' /><span>Indices</span></MenuItem>
+        <MenuItem onClick={handleNavClick} component={Link} to='/issue-feedback' className={handleClassName(menus.issueAndFeedback)}><ChatIcon className='menuicon' /><span>Issue/Feedback</span></MenuItem>
       </MenuList>
       <SocialIcons />
     </div>

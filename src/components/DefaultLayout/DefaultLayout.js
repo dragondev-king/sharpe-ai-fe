1ae6fd6f0e6e3ea525d7ac8c86019f1bf74c42f8
dragondev-king <dragondev1017@gmail.com>
2022-05-26 00:01:53 +0900

    remove commented lines in code

--- src/components/DefaultLayout/DefaultLayout.js
+++ src/components/DefaultLayout/DefaultLayout.js
@@ -1,18 +1,9 @@
-// import { useState, useCallback } from "react"
-
 import Header from '../Header'
 import Navbar from "../Navbar"
 import './style.scss'
 
 
 const DefaultLayout = ({component}) => {
-  // const [subIsOpened, setSubIsOpened] = useState(false)
-
-  // const handleSubClick = useCallback(() => {
-  //   setSubIsOpened(!subIsOpened)
-  //   console.log(subIsOpened, 'clicked')
-  // }, [subIsOpened, setSubIsOpened])
-
   return (
     <>
       <Header />

a7c3e0b7f823cf409063810be4a874b23d3e2dab
dragondev-king <dragondev1017@gmail.com>
2022-05-29 18:48:28 +0900

    show coming soon text in Centauri, Holios, Vela page

---
+++ src/routes/Stratigies/Routes/Helios/Helios.js
@@ -0,0 +1,18 @@
+import DefaultLayout from "../../../../components/DefaultLayout"
+import ComingSoonSimpleCard from "../../../../components/ComingSoonSimpleCard"
+
+const HeliosContent = () => {
+  return (
+    <>
+      <ComingSoonSimpleCard />
+    </>
+  )
+}
+
+const Helios = () => {
+  return (
+    <DefaultLayout component={<HeliosContent />} />
+  )
+}
+
+export default Helios
--- src/routes/Stratigies/Stratigies.js
+++ src/routes/Stratigies/Stratigies.js
@@ -2,12 +2,18 @@ import { Routes, Route } from "react-router-dom";
 
 import Taurus from "./Routes/Taurus";
 import Phantom from './Routes/Phantom';
+import Helios from "./Routes/Helios";
+import Vela from "./Routes/Vela";
+import Centauri from "./Routes/Centauri";
 
 const Stratigies = () => {
   return (
     <Routes>
       <Route path="/taurus" element={<Taurus />} />
       <Route path="/phantom" element={<Phantom />} />
+      <Route path="/helios" element={<Helios />} />
+      <Route path="/vela" element={<Vela />} />
+      <Route path="/centauri" element={<Centauri />} />
       <Route path="*" element={<Taurus />} />
     </Routes>
   );
